3a34bf111d4ac7763c11349a831b72ceb56f351c
YashShinde29 <165694051+YashShinde29@users.noreply.github.com>
2025-02-08 12:22:18 +0530

    Update webpack.config.js

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -39,10 +39,8 @@ module.exports = {
     new CopyWebpackPlugin([{ from: "./src/images/transplant-icon.svg", to: "images/transplant-icon.svg" }]),
     new CopyWebpackPlugin([{ from: "./src/images/waiting-list-icon.svg", to: "images/waiting-list-icon.svg" }]),
     new CopyWebpackPlugin([{ from: "./src/images/mail-icon.svg", to: "images/mail-icon.svg" }]),
-    new CopyWebpackPlugin([{ from: "./src/images/Gummadi.png", to: "images/Gummadi.png" }]),
     new CopyWebpackPlugin([{ from: "./src/images/Boini.png", to: "images/Boini.png" }]),
-    new CopyWebpackPlugin([{ from: "./src/images/Rahul Sabinkar.png", to: "images/Rahul Sabinkar.png" }]),
-    new CopyWebpackPlugin([{ from: "./src/images/Sai Manikanta.png", to: "images/Sai Manikanta.png" }]),    
+    ,    
   ],
   devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
 };
